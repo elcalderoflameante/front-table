e3bd5e4c872f359b60021aed38dae6de2b03a57a
Diego Ramos <diegof.ramosv@gmail.com>
2025-05-23 22:12:29 -0500

    Se realiza cambios para que tome la api url desde el archivo .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.66:3001/api', // Ajusta según tu backend
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Funciones para Meseros.jsx
